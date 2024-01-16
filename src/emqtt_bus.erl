d331d5b4ed9367f2fb36c32fca412fe27e1a1c86
Feng Lee <feng@emqx.io>
2024-01-16 17:14:40 +0800

    feat(bus): send CONNECT/CONNACK packets

--- src/emqtt_bus.erl
+++ src/emqtt_bus.erl
@@ -18,15 +18,18 @@
 
 -include("emqtt_bus.hrl").
 
--export([ name/1
-		, open/1
-		, domain/1
-		, peers/1
-		, subscriptions/1
-		, publish/3
-		, subscribe/2
-		, close/1
+-export([ name/0, name/1
+		, open/0, open/1
+		, domain/0, domain/1
+		, peers/0, peers/1
+		, subscriptions/0, subscriptions/1
+		, publish/2, publish/3
+		, subscribe/1, subscribe/2
+		, close/0, close/1
 		]).
+
+%% for debug
+-export([state/0, state/1]).
 
 -export([ ensure_open/1
 		, start_link/1
@@ -45,46 +48,68 @@
         ]).
 
 -record(domain, {id, addr, ports}).
--record(state, {sock, domain, controlling_process,
-				peers = [], topic_filters = [], subscriptions = []}).
+-record(state, {sock, domain, controlling_process, peerId, peers = [], topic_filters = [], subscriptions = []}).
 
 -define(BASE_PORT, 10000).
 -define(PORT_NUM, 10).
 -define(MULTICAST_ADDR, {224,18,83,0}).
 
+name() -> name(0).
+
 name(DomainId) when ?IS_DOMAIN_ID(DomainId) ->
 	list_to_atom("MQTT-BUS:" ++ integer_to_list(DomainId)).
 
+open() -> open(0).
+
 open(DomainId) when ?IS_DOMAIN_ID(DomainId) ->
 	start_link(DomainId).
+
+domain() -> domain(0).
 
 domain(DomainId) when ?IS_DOMAIN_ID(DomainId) ->
 	domain(name(DomainId));
 domain(Bus) ->
 	gen_server:call(Bus, domain).
 
+peers() -> peers(0).
+
 peers(DomainId) when ?IS_DOMAIN_ID(DomainId) ->
 	peers(name(DomainId));
 peers(Bus) ->
 	gen_server:call(Bus, peers).
+
+subscriptions() -> subscriptions(0).
 
 subscriptions(DomainId) when ?IS_DOMAIN_ID(DomainId) ->
 	subscriptions(name(DomainId));
 subscriptions(Bus) ->
 	gen_server:call(Bus, subscriptions).
 
+publish(Topic, Payload) ->
+	publish(0, Topic, Payload).
+
 publish(DomainId, Topic, Payload) when ?IS_DOMAIN_ID(DomainId) ->
 	publish(name(DomainId), Topic, Payload);
 publish(Bus, Topic, Payload) ->
-	gen_server:call(Bus, {publish, iolist_to_binary(Topic), iolist_to_binary(Payload)}).
+	gen_server:call(Bus, {publish, bin(Topic), bin(Payload)}).
+
+subscribe(Topic) ->
+	subscribe(0, Topic).
 
 subscribe(DomainId, Topic) when ?IS_DOMAIN_ID(DomainId) ->
 	subscribe(name(DomainId), Topic);
 subscribe(Bus, Topic) ->
-	gen_server:call(Bus, {subscribe, iolist_to_binary(Topic)}).
+	gen_server:call(Bus, {subscribe, bin(Topic)}).
+
+close() -> close(0).
 
 close(DomainId) when ?IS_DOMAIN_ID(DomainId) ->
 	stop(name(DomainId)).
+
+state() -> state(0).
+
+state(DomainId) ->
+	gen_server:call(name(DomainId), state).
 
 ensure_open(DomainId) ->
 	case start_link(DomainId) of
@@ -110,7 +135,9 @@ init([DomainId]) ->
 	Domain = #domain{id = DomainId, addr = MulticastAddr, ports = Ports},
 	case emqtt_bus_udp:open(Ports, MulticastAddr) of
 		{ok, Sock} ->
-			State = #state{domain = Domain, sock = Sock},
+			{ok, {_, Port}} = inet:sockname(Sock),
+			PeerId = generate_peerId(DomainId, Port),
+			State = #state{sock = Sock, domain = Domain, peerId = PeerId},
 			{ok, State, {continue, discover}};
         {error, Error} -> {stop, Error}
     end.
@@ -118,11 +145,27 @@ init([DomainId]) ->
 multicast_addr(DomainId) ->
     {A, B, C, D} = ?MULTICAST_ADDR, {A, B, C, D + DomainId}.
 
+generate_peerId(DomainId, Port) ->
+	{ok, Hostname} = inet:gethostname(),
+	bin([string:to_lower(Hostname), ":",
+		 integer_to_list(DomainId), ":",
+		 integer_to_list(Port)
+		]).
+
 handle_continue(discover, State = #state{sock = Sock, domain = #domain{addr = Addr, ports = Ports}}) ->
 	Packet = ?PACKET(?PINGREQ),
     lists:foreach(fun(Port) ->
 					  emqtt_bus_udp:send(Sock, Addr, Port, Packet)
 				  end, Ports),
+	{noreply, State};
+
+handle_continue({send_connect, Addr, Port}, State = #state{sock = Sock, peerId = PeerId}) ->
+	Packet = ?CONNECT_PACKET(#mqtt_packet_connect{clientid = PeerId}),
+	emqtt_bus_udp:send(Sock, Addr, Port, Packet),
+	{noreply, State};
+
+handle_continue({send_connack, Addr, Port}, State = #state{sock = Sock}) ->
+	emqtt_bus_udp:send(Sock, Addr, Port, ?CONNACK_PACKET(0)),
 	{noreply, State};
 
 handle_continue({send_subscribe, _Addr, _Port}, State = #state{topic_filters = []}) ->
@@ -133,20 +176,6 @@ handle_continue({send_subscribe, Addr, Port}, State = #state{sock = Sock, topic_
 	emqtt_bus_udp:send(Sock, Addr, Port, Packet),
 	{noreply, State}.
 
-handle_call({controlling_process, Pid}, _From, State) ->
-	{reply, ok, State#state{controlling_process = Pid}};
-
-handle_call(Publish = {publish, _Topic, _Payload}, _From, State) ->
-	handle_info(Publish, State),
-    {reply, ok, State};
-
-handle_call({subscribe, TopicFilter}, _From, State = #state{sock = Sock, peers = Peers, topic_filters = TopicFilters}) ->
-    Packet = ?SUBSCRIBE_PACKET(1, [{TopicFilter, #{qos => 0}}]),
-    lists:foreach(fun({Addr, Port}) ->
-					  emqtt_bus_udp:send(Sock, Addr, Port, Packet)
-                  end, Peers),
-    {reply, ok, State#state{topic_filters = lists:usort([TopicFilter|TopicFilters])}};
-
 handle_call(domain, _From, State = #state{domain = Domain}) ->
 	{reply, Domain, State};
 
@@ -155,6 +184,23 @@ handle_call(peers, _From, State = #state{peers = Peers}) ->
 
 handle_call(subscriptions, _From, State = #state{subscriptions = Subscriptions}) ->
 	{reply, Subscriptions, State};
+
+handle_call({controlling_process, Pid}, _From, State) ->
+	{reply, ok, State#state{controlling_process = Pid}};
+
+handle_call(state, _From, State) ->
+	{reply, State, State};
+
+handle_call(Publish = {publish, _Topic, _Payload}, _From, State) ->
+	handle_info(Publish, State),
+    {reply, ok, State};
+
+handle_call({subscribe, TopicFilter}, _From, State = #state{sock = Sock, peers = Peers, topic_filters = TopicFilters}) ->
+    Packet = ?SUBSCRIBE_PACKET(1, [{TopicFilter, #{qos => 0}}]),
+    lists:foreach(fun({_PeerId, Addr, Port}) ->
+					  emqtt_bus_udp:send(Sock, Addr, Port, Packet)
+                  end, Peers),
+    {reply, ok, State#state{topic_filters = lists:usort([TopicFilter|TopicFilters])}};
 
 handle_call(Req, _From, State) ->
     logger:error("Unexpected call: ~p", [Req]),
@@ -200,11 +246,20 @@ code_change(_OldVsn, State, _Extra) ->
 handle_incoming(Addr, Port, ?PACKET(?PINGREQ), State = #state{sock = Sock}) ->
 	io:format("PINGREQ from ~s:~p~n", [inet:ntoa(Addr), Port]),
 	emqtt_bus_udp:send(Sock, Addr, Port, ?PACKET(?PINGRESP)),
-	{noreply, State};
+	{noreply, State, {continue, {send_connect, Addr, Port}}};
 
 handle_incoming(Addr, Port, ?PACKET(?PINGRESP), State) ->
 	io:format("PINGRESP from ~s:~p~n", [inet:ntoa(Addr), Port]),
-	{noreply, add_peer({Addr, Port}, State), {continue, {send_subscribe, Addr, Port}}};
+	{noreply, State, {continue, {send_connect, Addr, Port}}};
+
+handle_incoming(Addr, Port, ?CONNECT_PACKET(Packet), State) ->
+	#mqtt_packet_connect{clientid = PeerId} = Packet,
+	io:format("CONNECT from ~s:~p - ~s~n", [inet:ntoa(Addr), Port, PeerId]),
+	{noreply, add_peer({PeerId, Addr, Port}, State), {continue, {send_connack, Addr, Port}}};
+
+handle_incoming(Addr, Port, ?CONNACK_PACKET(_), State) ->
+	io:format("CONNACK from ~s:~p~n", [inet:ntoa(Addr), Port]),
+	{noreply, State, {continue, {send_subscribe, Addr, Port}}};
 
 handle_incoming(Addr, Port, ?PUBLISH_PACKET(_QoS, Topic, _PacketId, Payload), State) ->
 	io:format("PUBLISH from ~s:~p -> ~p: ~p~n", [inet:ntoa(Addr), Port, Topic, Payload]),
@@ -215,7 +270,9 @@ handle_incoming(Addr, Port, ?PUBLISH_PACKET(_QoS, Topic, _PacketId, Payload), St
 	{noreply, State};
 
 handle_incoming(Addr, Port, ?SUBSCRIBE_PACKET(PacketId, TopicFilters), State = #state{sock = Sock}) ->
-	io:format("SUBSCRIBE from ~s:~p: ~p~n", [inet:ntoa(Addr), Port, TopicFilters]),
+	lists:foreach(fun({Filter, _Props}) ->
+					  io:format("SUBSCRIBE from ~s:~p: ~p~n", [inet:ntoa(Addr), Port, Filter])
+				  end, TopicFilters),
 	ReasonCodes = [QoS || {_, #{qos := QoS}} <- TopicFilters],
 	emqtt_bus_udp:send(Sock, Addr, Port, ?SUBACK_PACKET(PacketId, ReasonCodes)),
 	Subscriptions = [{Filter, {Addr, Port}} || {Filter, _} <- TopicFilters],
@@ -230,8 +287,11 @@ handle_incoming(Addr, Port, Packet, State) ->
 	{noreply, State}.
 
 add_peer(Peer, State = #state{peers = Peers}) ->
-	State#state{peers = lists:usort([Peer | Peers])}.
+	State#state{peers = lists:usort([Peer|Peers])}.
 
 add_subscriptions(New, State = #state{subscriptions = Existed}) ->
 	State#state{subscriptions = lists:usort(New ++ Existed)}.
+
+-compile({inline, [bin/1]}).
+bin(L) -> iolist_to_binary(L).
 
